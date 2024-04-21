b6c1f4e8d859ff5e7a5c2d7a52e9e779b6f957a8
Nirasha Jayalath <64356062+jayalath-jknr@users.noreply.github.com>
2024-04-21 12:27:02 +0530

    Update configuration.ts

--- biology-tutor-v4/src/configuration.ts
+++ biology-tutor-v4/src/configuration.ts
@@ -1,10 +1,1 @@
-import { Config } from "./contexts/ConfigurationContext";
 
-export const configuration: Config = {
-  corpusId: "4",
-  customerId: "3991645065",
-  appTitle: "biology-tutor-v4",
-  apiKey: "zqt_7euriXxmm9PxdU0GP3qSKK5OBWicvKB5ursfzg",
-  endpoint: "api.vectara.io",
-  questions: ["What is photosynthesis?"],
-};
